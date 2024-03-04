808496f866840fc17073b9d107d8737e22fe298a
Rutmauli <162088050+Rutmauli@users.noreply.github.com>
2024-03-04 19:15:59 +0700

    Add files via upload

---
+++ koneksi.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+
+import  java.sql.*;
+
+
+class koneksi {
+
+    private static Object Mahasiswa;
+    
+    public Statement st;
+    public ResultSet rs;
+    /*== <default package>.Mahasiswa.BukaKoneksi();
+    */    private static class KoneksiDatabase {
+
+        public KoneksiDatabase() {
+        }
+    }
+
+    private static class mahasiswa {
+
+        public mahasiswa() {
+        }
+    }
+
+    private static class Mahasiswa {
+
+        public Mahasiswa() {
+        }
+
+        private static class BukaKoneksi {
+
+            public BukaKoneksi() {
+            }
+        }
+    }
+    
+}
